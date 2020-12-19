538dc3687e4c4aaee9a227cfe8797a96ddcb00cd
FongHou <you@example.com>
2020-12-18 20:03:19 -0500

    how to use aws-sdk

--- src/lambda.cljs
+++ src/lambda.cljs
@@ -1,7 +1,12 @@
 (ns lambda
-  (:require [cljsjs.console :refer [spy log]]))
+  (:require [cljsjs.console :refer [spy log]]
+            [oops.core :refer [oget oset!]]
+            [promesa.core :as p]))
 
-#trace
+(def ^js aws (js/require "aws-sdk"))
+(def ^js s3 (new (.-S3 aws)))
+(def ^js iam (new (.-IAM aws)))
+
 (defn handler [event context callback]
    (log event context)
     (callback
@@ -9,6 +14,11 @@
      #js{:statusCode 200
          :body        "Hello from CLJS Lambda!"
          :header      #js{}}))
+
+(p/plet [buckets (spy (.. s3 (listBuckets #js{}) promise))
+         roles (spy (.. iam (listRoles #js{}) promise))]
+  (println (oget buckets "Buckets"))
+  (println (oget roles "Roles.?10.RoleName")))
 
 (comment
 
