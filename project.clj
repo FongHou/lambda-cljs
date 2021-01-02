0b3a90494df007ec96c42be8d44ab66557609ab5
FongHou <you@example.com>
2021-01-01 22:45:24 -0500

    update

--- project.clj
+++ project.clj
@@ -1,9 +1,11 @@
 (defproject lambda-cljs "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.2-alpha2"]
                  [org.clojure/clojurescript "1.10.773"]
+                 [org.clojure/core.async "1.3.610"]
                  [org.clojure/core.match "1.0.0"]
                  [appliedscience/js-interop "0.2.5"]
                  [binaryage/oops "0.7.0"]
+                 [com.wsscode/async "1.0.13"]
                  [com.wsscode/edn-json "1.1.0"]
                  [floatingpointio/graphql-builder "0.1.14"]
                  [funcool/cuerdas "2020.03.26-3"]
